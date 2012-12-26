4406157fb8db36ad7b0aa7e46913285c919d642a
Filype Pereira <pereira.filype@gmail.com>
2012-12-27 12:22:00 +1300

    ENHANCEMENT now everything on the backend has the default cakephp template

--- Controller/AppController.php
+++ Controller/AppController.php
@@ -32,4 +32,11 @@ App::uses('Controller', 'Controller');
  * @link http://book.cakephp.org/2.0/en/controllers.html#the-app-controller
  */
 class AppController extends Controller {
+
+	public function beforeFilter() {
+		if($this->request->admin === TRUE) {
+			$this->layout = 'admin';
+		}
+	}
+
 }
