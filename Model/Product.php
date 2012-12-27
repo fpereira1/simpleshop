ac130750585b78469313d9beca88d533b7029003
Filype Pereira <pereira.filype@gmail.com>
2012-12-28 00:29:30 +1300

    ENHANCEMENT The product object is loaded with extra fields including formatted properties

--- Model/Product.php
+++ Model/Product.php
@@ -1,5 +1,7 @@
 <?php
+
 App::uses('AppModel', 'Model');
+App::uses('CakeNumber', 'Utility');
 /**
  * Product Model
  *
@@ -47,5 +49,37 @@ class Product extends AppModel {
 	public $virtualFields = array(
 		'display_price' => 'IF(sale_price<price && sale_price>0,sale_price,price)'
 	);
+
+	public function beforeFind($query) {
+		$fakeRequest = new CakeRequest();
+		// Hack to get it to show hidden products in the backend
+		if(strpos($fakeRequest->url, 'admin') === FALSE) {
+			$query['conditions'] = am($query['conditions'], array(
+				'Product.hidden' => false
+			));
+		}
+		return $query;
+	}
+
+	public function afterFind($results, $primary = false) {
+		parent::afterFind($results, $primary);
+		if(isset($results[0]['Product'])) {
+			foreach ($results as &$item) {
+				$p = $item['Product'];
+				foreach ($item['Product'] as $key => $value) {
+					if(strpos($key, 'price') !== FALSE) {
+						$item['Product']['formatted_' . $key] = CakeNumber::currency($value, Configure::read('Shop.currency'));
+					}
+				}
+				$discount = 0;
+				if($p['sale_price'] > 0) {
+					$discount = (($p['price'] - $p['sale_price']) / $p['price']) * 100;
+				}
+				$item['Product']['discount'] = $discount;
+				$item['Product']['formatted_discount'] = CakeNumber::toPercentage($discount, 0);
+			}
+		}
+		return $results;
+	}
 
 }
