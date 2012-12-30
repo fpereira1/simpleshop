26ea9a9e1a1a477b3c1a8aee3636e4b2f9bd39c1
Filype Pereira <pereira.filype@gmail.com>
2012-12-30 21:06:50 +1300

    MINOR Adding Config files to the repository

---
+++ Config/Schema/schema.php
@@ -0,0 +1,24 @@
+<?php 
+class LaraSchema extends CakeSchema {
+
+	public function before($event = array()) {
+		return true;
+	}
+
+	public function after($event = array()) {
+	}
+
+	public $products = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary'),
+		'title' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 100, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'description' => array('type' => 'text', 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'price' => array('type' => 'float', 'null' => false, 'default' => null),
+		'sale_price' => array('type' => 'float', 'null' => false, 'default' => null),
+		'stockcount' => array('type' => 'integer', 'null' => false, 'default' => null),
+		'hidden' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 4),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1)
+		),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
+	);
+}
